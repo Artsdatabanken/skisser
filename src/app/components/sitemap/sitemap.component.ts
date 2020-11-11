fbd1f492fef7936c457dbc01595ffa901bfc7fd4
Constanza R Hauge <haugeconstanza@gmail.com>
2020-11-11 14:34:12 +0100

    New fonts, new sightings ++++

--- src/app/components/sitemap/sitemap.component.ts
+++ src/app/components/sitemap/sitemap.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-sitemap',
   templateUrl: './sitemap.component.html',
   styleUrls: ['./sitemap.component.scss']
 })
+
 export class SitemapComponent implements OnInit {
 
-  constructor() { }
+  menuItems: any[];
+
+  constructor(private navigationService: NavigationService) { }
 
   ngOnInit(): void {
+    this.menuItems = this.navigationService.getRoutes();
   }
 
 }
