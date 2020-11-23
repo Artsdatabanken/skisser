8e376d2e62ba801f68505167dc230b930557cec1
Constanza R Hauge <haugeconstanza@gmail.com>
2020-11-23 12:55:54 +0100

    Stats

--- src/app/components/statistics/overview/overview-child7/overview-child7.component.ts
+++ src/app/components/statistics/overview/overview-child7/overview-child7.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-overview-child7',
   templateUrl: './overview-child7.component.html',
   styleUrls: ['./overview-child7.component.scss']
 })
-export class OverviewChild7Component implements OnInit {
 
-  constructor() { }
+export class OverviewChild7Component implements OnInit {
+ 
+  pageTitle: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.pageTitle = this.route.routeConfig.data.text;
   }
 
 }
