2297fb1b7797cd35d703c8f2f642c4c2dd897ea7
Constanza R Hauge <haugeconstanza@gmail.com>
2021-01-18 19:45:05 +0100

    Endringer i tabeller for antall kvalitetssikrede obs per artsgruppe

--- artsobservasjoner/src/app/components/statistics/overview/overview-child5/overview-child5.component.ts
+++ artsobservasjoner/src/app/components/statistics/overview/overview-child5/overview-child5.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { formatNumber, registerLocaleData } from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 
@@ -12,21 +13,33 @@ export class OverviewChild5Component implements OnInit {
 
   pageTitle: string;
   sightings: any[] = [];
+  locale: any;
 
   constructor(
+    @Inject(LOCALE_ID) locale: string,
     private route: ActivatedRoute,
     private dataService: DataService
-  ) { }
+  ) { 
+    console.log('locale', locale);
+    this.locale = locale;
+  }
 
   ngOnInit(): void {
+
     this.pageTitle = this.route.routeConfig.data.text;
 
     this.dataService.getQAData().subscribe((res) => {
       this.sightings = res;
-      
+
       console.log('res', res);
       console.log('this.sightings', this.sightings);
+
+      this.sightings.forEach(s => {
+        console.log('T E S T', formatNumber(Number(s.SightingCount), this.locale, '1.0-0'))
+      })
+
     });
   }
+
 
 }
