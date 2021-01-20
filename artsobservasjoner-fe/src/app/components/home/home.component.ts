d96c7e74f0ed02d4ad964fbccb97bbfd5b21fc0b
Constanza R Hauge <haugeconstanza@gmail.com>
2021-01-20 11:40:26 +0100

    Start language switcher (utkommentert)

--- artsobservasjoner-fe/src/app/components/home/home.component.ts
+++ artsobservasjoner-fe/src/app/components/home/home.component.ts
@@ -9,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   loremIpsum: string = 'Etiam eleifend felis sed tristique suscipit. Nullam accumsan laoreet velit, sagittis in at nullam molestie placerat ex';
+  
+  // siteLanguage: string;
+  // siteLocale: string;
+  // languageList = [
+  //   { code: 'no', label: 'Norsk' },
+  //   { code: 'en', label: 'English' },
+  //   { code: 'es', label: 'Español' },
+  // ]
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    
+    // this.siteLocale = window.location.pathname.split('/')[1];
+
+    // this.siteLanguage = this.languageList.find(
+    //   (f) => {
+    //     f.code === this.siteLocale;
+    //   }
+    // )?.label;
+
+    // if (!this.siteLanguage) {
+    //   //this.onChange(this.languageList[0].code)
+    // }
+
+
+    
+    
+  }
+
+  onChange(selectedLangCode: string) {
+    window.location.href = `/${selectedLangCode}`
+  }
 
 }
