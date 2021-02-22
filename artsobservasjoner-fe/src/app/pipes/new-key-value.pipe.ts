4679d695d4923cd459a2a18cc422feb5fcaa8a76
Constanza R Hauge <haugeconstanza@gmail.com>
2021-02-22 10:54:13 +0100

    Modifiserte og forenklet tabellkode (CSS)

--- artsobservasjoner-fe/src/app/pipes/new-key-value.pipe.ts
+++ artsobservasjoner-fe/src/app/pipes/new-key-value.pipe.ts
@@ -1,7 +1,7 @@
 import { KeyValuePipe } from '@angular/common';
 import { KeyValueDiffers, Pipe, PipeTransform } from '@angular/core';
 
-const unordered = (a: any, b: any) => a;
+const UNORDERED = (a: any, b: any) => a;
 
 @Pipe({
   name: 'newKeyValue'
@@ -15,7 +15,7 @@ export class NewKeyValuePipe implements PipeTransform {
 
   constructor(public differs: KeyValueDiffers) { }
 
-  public transform(value, compareFn = unordered): unknown {
+  public transform(value, compareFn = UNORDERED): unknown {
     const pipe = new KeyValuePipe(this.differs);
     return pipe.transform(value, compareFn);
   }
