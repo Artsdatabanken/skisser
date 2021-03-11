fe4d27f0a04953c4991c1a469ec6793d54ff622a
Constanza R Hauge <haugeconstanza@gmail.com>
2021-03-11 10:22:56 +0100

    Sluttsføring arbeid med tabell artsobs vs artskart

--- artsobservasjoner-fe/src/app/components/statistics/overview/overview-child3/overview-child3.component.ts
+++ artsobservasjoner-fe/src/app/components/statistics/overview/overview-child3/overview-child3.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { forkJoin, Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { SIGHTINGS_PER_YEAR, StatisticsItem } from 'src/app/models/statistics';
+import { GRAPHCOLORS } from 'src/app/data/graphs';
+import { SIGHTINGS_PER_YEAR } from 'src/app/models/statistics';
 import { LayoutService } from 'src/app/services/layout.service';
 import { StatisticsService } from 'src/app/services/statistics.service';
 import { TranslationService } from 'src/app/services/translation.service';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-overview-child3',
@@ -20,6 +23,14 @@ export class OverviewChild3Component implements OnInit {
   subscription: Subscription;
   sightingsCountPerYear: typeof SIGHTINGS_PER_YEAR = SIGHTINGS_PER_YEAR;
 
+  @ViewChild('myCanvas') canvasRef: ElementRef;
+  chart: any[] = [];
+
+  graphLabels: string[] = [];
+  graphValues1: number[] = [];
+  graphValues2: number[] = [];
+  graphColors: string[] = GRAPHCOLORS;
+
   constructor(
     private layoutService: LayoutService,
     private statisticsService: StatisticsService,
@@ -32,6 +43,7 @@ export class OverviewChild3Component implements OnInit {
     this.currentLanguage$ = this.translationService.currentLanguage$;
     this.getData();
 
+    console.log('graf', this.canvasRef)
   }
 
   getData(): void {
@@ -59,16 +71,76 @@ export class OverviewChild3Component implements OnInit {
 
             }
 
+            if (!this.graphValues2.includes(elem.count)) {
+              this.graphValues2.push(elem.count);
+            }
+
           });
+
+          if (!this.graphLabels.includes(element.id.toString())) {
+            this.graphLabels.push(element.id.toString());
+          }
+
+          if (!this.graphValues1.includes(element.count)) {
+            this.graphValues1.push(element.count);
+          }
 
         });
 
-        console.log('temp', tempArray)
+        console.log('temp', this.graphValues1)
+        console.log('temp', this.graphValues2)
+        this.buildChart(this.graphLabels, this.graphValues1, this.graphValues2);
         return tempArray;
 
       })
     );
 
+
+  }
+
+  buildChart(labels: string[], gValues1: number[], gValues2: number[]): void {
+
+    Chart.defaults.global.defaultFontFamily = 'zabal';
+    Chart.defaults.global.defaultFontColor = 'black';
+    Chart.defaults.global.defaultFontStyle = '500';
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.legend.position = 'left';
+
+    this.chart = new Chart('myCanvas', {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: gValues1,
+            label: 'Artsobservasjoner',
+            borderColor: this.graphColors[7],
+            borderWidth: 2,
+            fill: false
+          },
+          {
+            data: gValues2,
+            label: 'Artskart',
+            borderColor: this.graphColors[5],
+            borderWidth: 2,
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: true
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: false
+          }],
+        }
+      }
+    });
 
   }
 
