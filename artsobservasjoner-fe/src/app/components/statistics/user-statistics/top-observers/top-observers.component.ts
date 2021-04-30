4fbbd5777f83b013cbb069c9fdc1601902cb6d2d
Constanza Hauge <constanza.hauge@bouvet.no>
2021-04-30 16:47:42 +0200

    Refactorering user stats, top observers m.m.

--- artsobservasjoner-fe/src/app/components/statistics/user-statistics/top-observers/top-observers.component.ts
+++ artsobservasjoner-fe/src/app/components/statistics/user-statistics/top-observers/top-observers.component.ts
@@ -6,6 +6,7 @@ import { LayoutService } from 'src/app/services/layout.service';
 import { SpeciesService } from 'src/app/services/species.service';
 import { TranslationService } from 'src/app/services/translation.service';
 import { UserStatisticsService } from 'src/app/services/user-statistics.service';
+import { UtilitiesService } from 'src/app/services/utilities.service';
 
 const PAGE_SIZE: number = 20;
 
@@ -22,14 +23,21 @@ export class TopObserversComponent implements OnInit {
   public totalCountStatistics: typeof TOTAL_COUNT_STATISTICS = TOTAL_COUNT_STATISTICS;
   userStatistics$: Observable<UserStatistics>;
   totalPages$: BehaviorSubject<number>;
-  speciesGroups$: Observable<Category[]>;
-  selectedSpeciesGroup: number | null = null;
   position: number;
+  speciesGroups$: Observable<Category[]>;
+  years: number[];
+
+
+  selectedYear: number | null = null;
+  selectedSpeciesGroup: number | null = null;
+  selectedTaxon: number | null = null;
+  selectedArea: number | null = null;
 
   constructor(
     private layoutService: LayoutService,
     private translationService: TranslationService,
     private speciesService: SpeciesService,
+    private utilitiesService: UtilitiesService,
     private userStatisticsService: UserStatisticsService
   ) {
     this.pageTitle$ = this.layoutService.setPageTitle('menu.menu_statistics_userStatistics_topObservers');
@@ -39,18 +47,24 @@ export class TopObserversComponent implements OnInit {
   ngOnInit(): void {
 
     this.speciesGroups$ = this.speciesService.speciesGroups;
+    this.years = this.utilitiesService.generateYears();
     this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(1, PAGE_SIZE);
     this.totalPages$ = this.userStatisticsService.totalPages$;
 
   }
 
   onPageChange(event: number): void {
-    this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(event, PAGE_SIZE, this.selectedSpeciesGroup);
+    this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(event, PAGE_SIZE, this.selectedYear, this.selectedSpeciesGroup, this.selectedTaxon, this.selectedArea);
   }
 
   onSpeciesGroupSelection(event: Event): void {
-    this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(1, PAGE_SIZE, this.selectedSpeciesGroup);
+    this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(1, PAGE_SIZE, this.selectedYear, this.selectedSpeciesGroup, this.selectedTaxon, this.selectedArea);
   }
+
+  onYearSelection(event: Event): void {
+    console.log('selected year', event)
+    this.userStatistics$ = this.userStatisticsService.getTopUsersStatistics(1, PAGE_SIZE, this.selectedYear, this.selectedSpeciesGroup, this.selectedTaxon, this.selectedArea);
+ }
 
   getPosition(index: number, pageNumber: number, pageSize: number): number {
 
--- artsobservasjoner-fe/src/app/services/user-statistics.service.ts
+++ artsobservasjoner-fe/src/app/services/user-statistics.service.ts
@@ -10,27 +10,73 @@ import { TopObserver, UserStatistics } from '../models/statistics';
 
 export class UserStatisticsService {
 
-  TOPOBSERVERS_API: string = 'https://ao3-statisticsapi-test.azurewebsites.net/api/v1/TopList/ObserverSpeciesCount?';
   totalPages$ = new BehaviorSubject(0);
 
   constructor(private httpClient: HttpClient) { }
 
-  getTopUsersStatistics(pageNumber: number = 1, pageSize: number, speciesGroupId?: number, year?: number): Observable<UserStatistics> {
+  getTopUsersStatistics(
+    pageNumberParam: number = 1,
+    pageSizeParam: number,
+    yearParam?: number,
+    speciesGroupParam?: number,
+    taxonParam?: number,
+    areaParam?: number): Observable<UserStatistics> {
 
-    console.log('service', pageSize)
+    const baseUrl: string = 'https://ao3-statisticsapi-test.azurewebsites.net/api/v1/TopList/ObserverSpeciesCount?';
     let api: string;
+    let params: URLSearchParams = new URLSearchParams();
 
-    if (speciesGroupId) {
-      api = this.TOPOBSERVERS_API + 'SpeciesGroupId=' + speciesGroupId + '&PageNumber=' + pageNumber + '&PageSize=' + pageSize;
-    }
-    else if (year) {
-      api = this.TOPOBSERVERS_API + 'Year=' + year + 'PageNumber=' + pageNumber + '&PageSize=' + pageSize;
-    }
-    else {
-      api = this.TOPOBSERVERS_API + 'PageNumber=' + pageNumber + '&PageSize=' + pageSize;
-    }
+    console.log('params XXX', yearParam);
+    console.log('params XXX', speciesGroupParam);
+    console.log('params XXX', taxonParam);
+    console.log('params XXX', areaParam);
+    console.log('params XXX', pageNumberParam);
+    console.log('params XXX', pageSizeParam);
 
-    // console.log('api', api)
+    if (yearParam) params.append('Year', yearParam.toString());
+    if (speciesGroupParam) params.append('SpeciesGroupId', speciesGroupParam.toString());
+    if (taxonParam) params.append('TaxonId', taxonParam.toString());
+    if (areaParam) params.append('AreaId', areaParam.toString());
+    if (pageNumberParam) params.append('PageNumber', pageNumberParam.toString());
+    if (pageSizeParam) params.append('PageSize', pageSizeParam.toString());
+
+    api = baseUrl + params.toString();
+
+    // const params: URLSearchParams = new URLSearchParams({
+    //   year: yearParam?.toString(),
+    //   speciesGroupId: speciesGroupParam?.toString(),
+    //   taxonId: taxonParam?.toString(),
+    //   areaId: areaParam?.toString(),
+    //   pageNumber: pageNumberParam.toString(),
+    //   pageSize: pageSizeParam.toString()
+    // });
+
+    console.log('params 1', params.toString());
+
+    // for (var pair of params.entries()) {
+    //   console.log('KEY', pair[0]);
+    //   console.log('VALUE', pair[1]);
+    //   //console.log('ENTRIES', pair[0] + ', ' + pair[1]);
+
+    //   if (params.get(pair[0]) == undefined) {
+    //     params.delete(pair[0]);
+    //   }
+
+    // }
+
+    console.log('params 2', params.toString());
+
+    // if (speciesGroupId) {
+    //   api = this.TOPOBSERVERS_API + 'SpeciesGroupId=' + speciesGroupId + '&PageNumber=' + pageNumber + '&PageSize=' + pageSize;
+    // }
+    // else if (year) {
+    //   api = this.TOPOBSERVERS_API + 'Year=' + year + 'PageNumber=' + pageNumber + '&PageSize=' + pageSize;
+    // }
+    // else {
+    //   api = this.TOPOBSERVERS_API + 'PageNumber=' + pageNumber + '&PageSize=' + pageSize;
+    // }
+
+    console.log('api', api)
 
     return this.httpClient.get(api).pipe(
       map((response: any) => {
@@ -59,7 +105,7 @@ export class UserStatisticsService {
           totalCount: response.totalCount
         }
 
-        this.totalPages$.next(Math.ceil(response.totalCount / pageSize)); 
+        this.totalPages$.next(Math.ceil(response.totalCount / pageSizeParam));
 
         //console.log('userStatisticsObject', userStatisticsObject)
         return userStatisticsObject;
